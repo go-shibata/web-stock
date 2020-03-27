e6cac5dbfd58c553b68d531d0ff5627fc1737206
go shibata <warahiko@gmail.com>
2020-03-27 17:36:39 +0900

    Update: layout of LinkItem Update: add variable to Link

--- app/src/main/java/com/example/go/webstockapp/database/entity/Link.kt
+++ app/src/main/java/com/example/go/webstockapp/database/entity/Link.kt
@@ -9,9 +9,16 @@ data class Link(
     @PrimaryKey(autoGenerate = true)
     var id: Long,
 
+    var title: String,
     var url: String,
     var savedDate: Date,
     var isChecked: Boolean = false,
     var checkedDate: Date? = null,
     var notifyDate: Date? = null
-)
+) {
+    val domain: String?
+        get() {
+            val regex = Regex("//([a-z0-9.]+)")
+            return regex.find(url)?.groupValues?.get(1)
+        }
+}
