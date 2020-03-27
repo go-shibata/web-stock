5bab05700ac92285065345b951e5a45279336374
go shibata <warahiko@gmail.com>
2020-03-27 22:12:08 +0900

    Update: fetch title of Link

--- app/src/main/java/com/example/go/webstockapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/go/webstockapp/ui/home/HomeViewModel.kt
@@ -5,6 +5,12 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.example.go.webstockapp.database.MyDatabase
 import com.example.go.webstockapp.database.entity.Link
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import java.util.*
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
@@ -12,4 +18,19 @@ class HomeViewModel @Inject constructor(
 ) : AndroidViewModel(app) {
 
     val links: LiveData<List<Link>> = MyDatabase.getInstance(app).linkDao().getAllLinks()
+
+    fun insertLink(url: String) {
+        CoroutineScope(Dispatchers.Default).launch {
+            val title = withContext(Dispatchers.IO) {
+                Jsoup.connect(url)
+                    .get()
+                    .getElementsByTag("title")
+                    .text()
+            }
+            val link = Link(title, url, Date(System.currentTimeMillis()))
+            MyDatabase.getInstance(getApplication())
+                .linkDao()
+                .insert(link)
+        }
+    }
 }
