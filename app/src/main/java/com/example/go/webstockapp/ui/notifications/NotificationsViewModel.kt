dac5ddc05fb9400d00d69485fb1593b59c26eb17
go shibata <warahiko@gmail.com>
2020-03-31 05:00:57 +0900

    Update: delete notification

--- app/src/main/java/com/example/go/webstockapp/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/go/webstockapp/ui/notifications/NotificationsViewModel.kt
@@ -2,7 +2,12 @@ package com.example.go.webstockapp.ui.notifications
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.work.WorkManager
 import com.example.go.webstockapp.database.MyDatabase
+import com.example.go.webstockapp.database.entity.Notification
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NotificationsViewModel @Inject constructor(
@@ -10,4 +15,22 @@ class NotificationsViewModel @Inject constructor(
 ) : AndroidViewModel(app) {
 
     val notifications = MyDatabase.getInstance(app).notificationDao().getAllNotificationWithLink()
+
+    fun editNotification(notification: NotificationAndLink) {
+        TODO()
+    }
+
+    fun deleteNotification(notification: NotificationAndLink) {
+        cancelNotification(notification.notification)
+        CoroutineScope(Dispatchers.Default).launch {
+            MyDatabase.getInstance(getApplication())
+                .notificationDao()
+                .delete(notification.notification)
+        }
+    }
+
+    private fun cancelNotification(notification: Notification) {
+        WorkManager.getInstance()
+            .cancelWorkById(notification.requestId)
+    }
 }
