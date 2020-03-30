a4ba87b583b7c25b691b152c1e44006e963dcbbb
go shibata <warahiko@gmail.com>
2020-03-31 05:09:32 +0900

    Update: edit notification

--- app/src/main/java/com/example/go/webstockapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/go/webstockapp/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.go.webstockapp.ui.notifications
 
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.go.webstockapp.R
 import com.example.go.webstockapp.databinding.FragmentNotificationsBinding
 import com.example.go.webstockapp.di.ViewModelFactory
+import com.example.go.webstockapp.ui.MyDateTimePickerDialog
 import com.example.go.webstockapp.ui.MyDialogFragment
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -19,6 +22,8 @@ class NotificationsFragment : Fragment() {
 
     @Inject
     lateinit var factory: ViewModelFactory<NotificationsViewModel>
+
+    private lateinit var binding: FragmentNotificationsBinding
 
     private val viewModel: NotificationsViewModel by activityViewModels { factory }
     private val onClickNotificationListener =
@@ -33,7 +38,7 @@ class NotificationsFragment : Fragment() {
                     )
                     .setItems(R.array.click_notification_list) { dialog, which ->
                         when (which) {
-                            0 -> viewModel.editNotification(notification)
+                            0 -> editNotification(notification)
                             1 -> viewModel.deleteNotification(notification)
                         }
                         dialog.dismiss()
@@ -55,7 +60,7 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.notificationList.apply {
             adapter = NotificationListAdapter(this@NotificationsFragment, viewModel).apply {
@@ -64,5 +69,30 @@ class NotificationsFragment : Fragment() {
             layoutManager = LinearLayoutManager(context)
         }
         return binding.root
+    }
+
+    fun editNotification(notification: NotificationAndLink) {
+        val onDateTimeSetListener = object : MyDateTimePickerDialog.OnDateTimeSetListener {
+            override fun onDateTimeSet(calendar: Calendar) {
+                Snackbar.make(
+                    binding.root,
+                    getString(
+                        R.string.notification_create_message,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH),
+                        calendar.get(Calendar.HOUR_OF_DAY),
+                        calendar.get(Calendar.MINUTE)
+                    ),
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                viewModel.editNotification(notification, calendar.timeInMillis)
+            }
+        }
+        MyDateTimePickerDialog(
+            parentFragmentManager
+        ).apply {
+            setOnDateTimeSetListener(onDateTimeSetListener)
+        }.show()
     }
 }
--- app/src/main/java/com/example/go/webstockapp/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/go/webstockapp/ui/notifications/NotificationsViewModel.kt
@@ -2,12 +2,18 @@ package com.example.go.webstockapp.ui.notifications
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.go.webstockapp.database.MyDatabase
 import com.example.go.webstockapp.database.entity.Notification
+import com.example.go.webstockapp.work.NotificationWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class NotificationsViewModel @Inject constructor(
@@ -16,8 +22,34 @@ class NotificationsViewModel @Inject constructor(
 
     val notifications = MyDatabase.getInstance(app).notificationDao().getAllNotificationWithLink()
 
-    fun editNotification(notification: NotificationAndLink) {
-        TODO()
+    fun editNotification(notification: NotificationAndLink, timeInMillis: Long) {
+        cancelNotification(notification.notification)
+
+        val link = notification.getLink()
+        val request = OneTimeWorkRequestBuilder<NotificationWorker>()
+            .setInputData(
+                Data.Builder()
+                    .putString(NotificationWorker.KEY_TITLE, link.title)
+                    .putString(NotificationWorker.KEY_URL, link.url)
+                    .putLong(NotificationWorker.KEY_ID, link.id)
+                    .build()
+            )
+            .setInitialDelay(timeInMillis - System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+            .build()
+        WorkManager.getInstance()
+            .beginUniqueWork("notification", ExistingWorkPolicy.REPLACE, request)
+            .enqueue()
+
+        CoroutineScope(Dispatchers.Default).launch {
+            val item = notification.notification.copy(
+                savedDate = Date(System.currentTimeMillis()),
+                notifyDate = Date(timeInMillis),
+                isCompleted = false
+            )
+            MyDatabase.getInstance(getApplication())
+                .notificationDao()
+                .update(item)
+        }
     }
 
     fun deleteNotification(notification: NotificationAndLink) {
